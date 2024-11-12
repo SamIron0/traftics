3c8d691d9130e35e63691ec8e86daa6de7be739a
SamIron0 <samuelironkwec@gmail.com>
2024-11-12 12:41:40 -0600

    update

--- infrastructure/scripts/build-tracker.sh
+++ infrastructure/scripts/build-tracker.sh
@@ -4,10 +4,5 @@
 cd packages/tracker
 pnpm build
 
-# Upload to S3
-aws s3 cp dist/tracker.js s3://session-recorder-cdn/js/tracker.js
-
-# Invalidate CloudFront cache
-aws cloudfront create-invalidation \
-  --distribution-id E202403251234567890 \
-  --paths "/js/tracker.js"
+# Deploy to Vercel
+vercel deploy dist/tracker.js --prod
