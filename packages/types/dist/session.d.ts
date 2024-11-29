e697f731ab61aeb1f6c77b2279da81cbe314f687
SamIron0 <samuelironkwec@gmail.com>
2024-11-29 12:30:49 -0600

    x

---
+++ packages/types/dist/session.d.ts
@@ -0,0 +1,12 @@
+import type { eventWithTime } from '@rrweb/types';
+export interface Session {
+    id: string;
+    site_id: string;
+    started_at: number;
+    duration: number;
+    events: eventWithTime[];
+    user_agent: string;
+    screen_width: number;
+    screen_height: number;
+    screenshot?: string | null;
+}
